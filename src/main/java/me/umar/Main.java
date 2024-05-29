dbbd180852a47241ba639544b1c6d8a665ba2a89
UmarDevelopment <fgsruss@gmail.com>
2024-05-29 17:48:01 +0300

    create first bean

---
+++ src/main/java/me/umar/Main.java
@@ -0,0 +1,11 @@
+package me.umar;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String ... args){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+    }
+}
