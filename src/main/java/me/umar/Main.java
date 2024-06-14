47cca38656da9ab39e6e7ef09e68396eb31cc2c1
UmarDevelopment <fgsruss@gmail.com>
2024-06-14 15:06:55 +0300

    lesson 8

--- src/main/java/me/umar/Main.java
+++ src/main/java/me/umar/Main.java
@@ -5,10 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String ... args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
-        mp.playMusic();
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
-        System.out.println(testBean.getSomeint());
+        MusicPlayer mp1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer mp2 = context.getBean("musicPlayer", MusicPlayer.class);
+        TestBean testBean1 = context.getBean("testBean", TestBean.class);
+        TestBean testBean2 = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean1==testBean2);
+        System.out.println(mp1==mp2);
     }
 }
