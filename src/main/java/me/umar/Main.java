def2475dcd03caa814ce706f035eb58842672eb3
UmarDevelopment <fgsruss@gmail.com>
2024-06-18 21:04:34 +0300

    lesson 16 SPRING MVC

--- src/main/java/me/umar/Main.java
+++ src/main/java/me/umar/Main.java
@@ -5,11 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String... args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
-        for (int i = 0; i < 10; i++) {
-            mp.playMusic(MusicType.CLASSIC);
-        }
-        context.close();
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+//        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+//        for (int i = 0; i < 10; i++) {
+//            mp.playMusic(MusicType.CLASSIC);
+//        }
+//        context.close();
     }
 }
