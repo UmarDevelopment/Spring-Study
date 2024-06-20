9cbcd60f9649e05f4b31bbd440a397c352110469
UmarDevelopment <fgsruss@gmail.com>
2024-06-20 16:39:15 +0300

    change XML config to java code config

---
+++ src/main/java/me/umar/controllers/FirstController.java
@@ -0,0 +1,18 @@
+package me.umar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "first/hello";
+    }
+
+    @GetMapping("/bye")
+    public String sayBye(){
+        return "first/bye";
+    }
+}
