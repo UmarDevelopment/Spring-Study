553804396b1e80c59e208bbd8d0c7824f97bc757
UmarDevelopment <fgsruss@gmail.com>
2024-06-21 16:34:33 +0300

    lesson 20

--- src/main/java/me/umar/controllers/FirstController.java
+++ src/main/java/me/umar/controllers/FirstController.java
@@ -2,17 +2,24 @@ package me.umar.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println(name+" : "+surname);
         return "first/hello";
     }
 
     @GetMapping("/bye")
-    public String sayBye(){
+    public String sayBye(@RequestParam("name") String name){
+        System.out.println(name);
         return "first/bye";
     }
 }
