44006381f18750a7ef79b2203b06e19cd05dac72
UmarDevelopment <fgsruss@gmail.com>
2024-07-19 00:04:27 +0300

    lesson 22 for cycle thymeleaf

--- src/main/java/me/umar/controllers/FirstController.java
+++ src/main/java/me/umar/controllers/FirstController.java
@@ -1,11 +1,14 @@
 package me.umar.controllers;
 
+import me.umar.test.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class FirstController {
@@ -18,6 +21,18 @@ public class FirstController {
         model.addAttribute("message", message);
         return "first/hello";
     }
+
+    @GetMapping("/test")
+    public String test(Model model){
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(1, "Umar"));
+        people.add(new Person(2, "Oleg"));
+        people.add(new Person(3, "Nika"));
+        people.add(new Person(4, "Vika"));
+        model.addAttribute("people", people);
+        return "first/test";
+    }
+
 
     @GetMapping("/bye")
     public String sayBye(@RequestParam("name") String name){
